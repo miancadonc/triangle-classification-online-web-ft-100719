a860d5b88fe66ec35e2d86f036551e31a15bbb07
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-28 03:25:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,16 @@ class Triangle
     if self.sides.any?{|side| side <= 0} || self.sides.any?{|side| side >= (self.sides.sum - side)}
       raise TriangleError
     end
-    self.sides.reject{|side| side != @side_one}.size
+    
+    case self.sides.uniq.size
+    when 3
+      :scalene
+    when 2
+      :isoceles
+    when 1
+      :equilateral
+    end
+        
   end
   
   class TriangleError < StandardError
