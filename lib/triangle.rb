b83039cc5dd929fbfa36cb965bef7c78de145881
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-28 03:18:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   end
   
   def kind
-    if self.sides.any?{|side| side <= 0} || self.sides.any?{|side| side > self.sides.r.sum}
+    if self.sides.any?{|side| side <= 0} || self.sides.any?{|side| side > (self.sides.sum - side)}
       raise TriangleError
     end
   end
