4dde086de5590af2ea74d98f19789f9710c7c5b7
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-28 03:11:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,14 @@ class Triangle
     @side_three = side_three
   end
   
+  def sides
+    [@side_one, @side_two, @side_three]
+  end
+  
   def kind
-   
+    if self.sides.any?{|side| side =< 0}
+      raise TriangleError
+    end
   end
   
   class TriangleError < StandardError
